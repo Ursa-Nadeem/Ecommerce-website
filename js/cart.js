a022be1a6d3f9c23158726f0c5635d8a2424f78b
Ursa Nadeem <ursanadeem13@gmail.com>
2022-04-25 14:18:49 +0500

    commit

--- js/cart.js
+++ js/cart.js
@@ -1,91 +1,38 @@
+//total amount
+const product_total_amount=document.getElementById("total_amount");//total
 
-const product_total_amount = document.getElementById("total_amount");
-const decreament = () => {
-
-    var itemval = document.getElementById("textbox");
-    var itemprice = document.getElementById("itemprice");
-    console.log(itemprice)
-    if (itemval.value <= 0) {
-        itemval.value = 0;
-
-    }
-    else {
-        let products = localStorage.getItem("products");
-        const parsearr = JSON.parse(products)
-        parsearr.map((i) => {
-            document.getElementById("titleget").innerHTML = i.title;
-            document.getElementById("itemprice").innerHTML = i.price;
-
-            itemval.value = parseInt(itemval.value) - 1;
-            itemval.style.background = "#fff";
-            itemval.style.color = '#000';
-
-            let a = itemval.value;
-            let b = i.price;
-            let c = a * b;
-            itemprice.innerHTML = c;
-            product_total_amount.innerHTML = c;
-        })
-    }
+// local storage delete
+const delete_product=(itemIndex)=>{
+    let products=localStorage.getItem("products");
+    let parsearr=JSON.parse(products);
+    parsearr.splice(itemIndex,1);
+    localStorage.setItem('products',JSON.stringify(parsearr));
 }
+console.log("hhhh")
 
-const increament = () => {
-    var itemval = document.getElementById("textbox");
-    var itemprice = document.getElementById("itemprice");
-    if (itemval.value >= 5) {
-        itemval.value = 5;
-        alert("max 5 allowed");
-        itemval.style.background = "red";
-        itemval.style.color = '#fff';
-    }
-    else {
-        let products = localStorage.getItem("products");
-        const parsearr = JSON.parse(products)
-        let sum;
-        parsearr.map((i) => {
-            document.getElementById("titleget").innerHTML = i.title;
-            document.getElementById("itemprice").innerHTML = i.price;
-            itemval.value = parseInt(itemval.value) + 1;
-            let tprice = itemval.value * i.price
-            itemprice.innerHTML = itemval.value * i.price;
-
-            console.log(tprice)
-
-            product_total_amount.innerHTML = tprice;
-
-        })
-
-
-    }
-}
-
-// localStorage  delete
-const delete_product = (itemIndex) => {
-    const product = localStorage.getItem("products");
-    let parsearr = JSON.parse(product);
-
-    parsearr.splice(itemIndex, 1);
-    localStorage.setItem('products', JSON.stringify(parsearr));
-}
-
-// Add
-
-const product = localStorage.getItem("products");
-const parsearr = JSON.parse(product);
-parsearr.map((i) => {
-    let cart=document.getElementById("titleget");
-    let heading=document.createElement('h5');
-    let text=document.createTextNode(i.title);
-    let icon=document.createElement("i");
-    icon.setAttribute("class","fa fa-trash-o")
+// local storage Add
+let products=localStorage.getItem("products");
+const parsearr=JSON.parse(products);
+parsearr.map((i)=>{
+    //title 
+    let title=document.getElementById("titleget");
+    let h5=document.createElement('h5');
+    h5.style.marginTop="2px";
+    let span=document.createElement('span');
+    let p=document.createTextNode("$")
+    let value=document.createTextNode(i.price);
+    span.setAttribute("class","amount");
+    span.setAttribute("id","itemprice");
+    let titletext=document.createTextNode(i.title);
+    let icon= document.createElement('i');
+    icon.style.paddingRight="5px";
+    icon.setAttribute("class","fa fa-trash-o");
     icon.setAttribute("onclick","delete_product(this)")
-    heading.appendChild(icon)
-    heading.appendChild(text)
-    cart.appendChild(heading);
+    h5.appendChild(icon);
+    h5.appendChild(titletext);
+    title.appendChild(h5)
+    span.appendChild(p);
+    span.appendChild(value);
+    h5.appendChild(span);
+       
 })
-
-
-
-
-
-
